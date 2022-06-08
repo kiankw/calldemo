8d53c1063759526f12217f7ea24e7c60e968309c
kian <kiankwok@126.com>
2022-06-08 14:12:26 +0800

    test function as parameter

--- main.go
+++ main.go
@@ -1,100 +1,47 @@
 package main
 
-import (
-	"fmt"
-)
-
-func VirtualCall(a Animal) {
-	a.HelloAnimal()
-}
+import "fmt"
 
 func main() {
-	// Static call
-	A()
-	B()
-	// Special call
-	var m Mouse = Mouse{Name: "Jerry"}
-	m.HelloMouse()
-	// Virtual call
-	var a Animal = Cat{Name: "Tom"}
-	a.HelloAnimal()
+	A(func(s string) {
+		fmt.Print("A: ")
+		fmt.Println(s)
+	})
 
-	// MyTestBranch(true)
+	p := People{Name: "Tom"}
+	p.B(func(b bool) {
+		fmt.Print("B: ")
+		fmt.Println(b)
+	})
 
-	MyPrint(HelloName)
-}
-
-func HelloName(name string) string {
-	return "Hello World!" + name
-}
-
-func MyPrint(f func(name string) string) {
-	fmt.Println(f("Tom"))
+	var a Animal
+	a = Cat{Name: "Tome"}
+	a.C(func(i int) {
+		fmt.Print("C: ")
+		fmt.Println(i)
+	})
 }
 
-func C() {
-	A()
-	B()
+func A(fp func(string)) {
+	fp("Hello")
 }
 
-func A() {
-	A1()
-	AB()
-	fmt.Println("Hello A")
+type People struct {
+	Name string
 }
-func B() {
-	B1()
-	AB()
-	fmt.Println("Hello B")
-}
-func A1() {
-	fmt.Println("Hello A1")
-}
-func AB() {
-	fmt.Println("Hello AB")
-}
-func B1() {
-	fmt.Println("Hello B1")
+
+func (p People) B(fp func(bool)) {
+	fp(true)
 }
 
 type Animal interface {
-	HelloAnimal()
+	C(fp func(int))
 }
 
 type Cat struct {
 	Name string
 }
 
-func (c Cat) HelloCat() {
-	fmt.Println("I am cat", c.Name)
-}
-func (c Cat) HelloAnimal() {
-	fmt.Println("I am animal cat", c.Name)
-}
-
-type Mouse struct {
-	Name string
-}
-
-func (m Mouse) HelloMouse() {
-	fmt.Println("I am mouse", m.Name)
-}
-func (m Mouse) HelloAnimal() {
-	fmt.Println("I am animal mouse", m.Name)
-}
-
-type Dog struct {
-	Name string
-}
-
-func (d Dog) HelloAnimal() {
-	fmt.Println("I am animal dog", d.Name)
-}
-
-func MyTestBranch(flag bool) {
-	if flag {
-		fmt.Println("True")
-	} else {
-		fmt.Println("False")
-	}
+func (c Cat) C(fp func(int)) {
+	fp(123)
 }
