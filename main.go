ab1d3a97441c3f3875d293c580862ef2435093af
kian <kiankwok@126.com>
2022-06-26 12:17:09 +0800

    add pkg load demo

--- main.go
+++ main.go
@@ -1,100 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+
+	"golang.org/x/tools/go/packages"
 )
 
-func VirtualCall(a Animal) {
-	a.HelloAnimal()
-}
-
 func main() {
-	// Static call
-	A()
-	B()
-	// Special call
-	var m Mouse = Mouse{Name: "Jerry"}
-	m.HelloMouse()
-	// Virtual call
-	var a Animal = Cat{Name: "Tom"}
-	a.HelloAnimal()
+	flag.Parse()
 
-	// MyTestBranch(true)
+	// Many tools pass their command-line arguments (after any flags)
+	// uninterpreted to packages.Load so that it can interpret them
+	// according to the conventions of the underlying build system.
+	cfg := &packages.Config{Mode: packages.NeedFiles | packages.NeedSyntax}
+	pkgs, err := packages.Load(cfg, flag.Args()...)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "load: %v\n", err)
+		os.Exit(1)
+	}
+	if packages.PrintErrors(pkgs) > 0 {
+		os.Exit(1)
+	}
 
-	MyPrint(HelloName)
-}
-
-func HelloName(name string) string {
-	return "Hello World!" + name
-}
-
-func MyPrint(f func(name string) string) {
-	fmt.Println(f("Tom"))
-}
-
-func C() {
-	A()
-	B()
-}
-
-func A() {
-	A1()
-	AB()
-	fmt.Println("Hello A")
-}
-func B() {
-	B1()
-	AB()
-	fmt.Println("Hello B")
-}
-func A1() {
-	fmt.Println("Hello A1")
-}
-func AB() {
-	fmt.Println("Hello AB")
-}
-func B1() {
-	fmt.Println("Hello B1")
-}
-
-type Animal interface {
-	HelloAnimal()
-}
-
-type Cat struct {
-	Name string
-}
-
-func (c Cat) HelloCat() {
-	fmt.Println("I am cat", c.Name)
-}
-func (c Cat) HelloAnimal() {
-	fmt.Println("I am animal cat", c.Name)
-}
-
-type Mouse struct {
-	Name string
-}
-
-func (m Mouse) HelloMouse() {
-	fmt.Println("I am mouse", m.Name)
-}
-func (m Mouse) HelloAnimal() {
-	fmt.Println("I am animal mouse", m.Name)
-}
-
-type Dog struct {
-	Name string
-}
-
-func (d Dog) HelloAnimal() {
-	fmt.Println("I am animal dog", d.Name)
-}
-
-func MyTestBranch(flag bool) {
-	if flag {
-		fmt.Println("True")
-	} else {
-		fmt.Println("False")
+	// Print the names of the source files
+	// for each package listed on the command line.
+	for _, pkg := range pkgs {
+		fmt.Println(pkg.ID, pkg.GoFiles)
 	}
 }
